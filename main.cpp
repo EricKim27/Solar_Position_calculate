5c0785e67d4db1e46d92424508a22d4198782f1c
Eric Kim <seohyun.kim@outlook.kr>
2024-05-08 18:55:37 +0900

    fixed some errors; more to be fixed.

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 
 using namespace space;
 using namespace std;
+
 const float dt=0.0625;
 
 int main()
@@ -13,14 +14,13 @@ int main()
     object Earth;
     object Sun;
     object Moon;
+    initvars();
     Sun.initialize_object
     (
         -1.191989205285392*1000*pow(10, 6), 
         -4.347654370292560*1000*pow(10, 5), 
         3.146124454779254*1000*pow(10, 4),
-        8.441016159655652*1000*pow(10,-3),
-        -1.221912155158748*1000*pow(10, -2),
-        -7.918737699351246*1000*pow(10, -5)
+        1
     );
 
     Earth.initialize_object
@@ -28,9 +28,7 @@ int main()
         -2.600298246493929*1000*pow(10, 7),
         1.445600958366426*1000*pow(10, 8),
         2.324604087693989*1000*pow(10, 4),
-        -2.983302263902714*1000*pow(10, 1),
-        -5.138481408411204*1000*pow(10, 0),
-        1.105037462794778*1000*pow(10, -3)
+        2
     );
 
     Moon.initialize_object
@@ -38,12 +36,10 @@ int main()
         -2.637093499608123*1000*pow(10, 7),
         1.447266276319263*1000*pow(10, 8),
         4.842335848964751*1000*10*pow(10, 4),
-        -3.024279048864922*1000*pow(10, 1),
-        -6.014108452553830*1000*pow(10, 0),
-        -5.816013309114965*1000*pow(10, -2)
+        3
     );
     //take in the simulation duration.
-    cout << "Input the simulation duration>";
+    cout << "Input the simulation duration> ";
     string input;
     cin >> input;
     int dur = stoi(input);
--- space.cpp
+++ space.cpp
@@ -1,39 +1,65 @@
 #include "space.hpp"
 #include <fstream>
 
+void space::initvars(){
+    space::vx_e=-2.983302263902714*1000*std::pow(10, 1);
+    space::vy_e=-5.138481408411204*1000*std::pow(10, 0);
+    space::vz_e = 1.105037462794778*1000*std::pow(10, -3);
+    space::vx_s=8.441016159655652*1000*std::pow(10,-3);
+    space::vy_s=-1.221912155158748*1000*std::pow(10, -2);
+    space::vz_s=-7.918737699351246*1000*std::pow(10, -5);
+    space::vx_m=-3.024279048864922*1000*std::pow(10, 1);
+    space::vy_m=-6.014108452553830*1000*std::pow(10, 0);
+    space::vz_m=-5.816013309114965*1000*std::pow(10, -2);
+    space::r_s=Eigen::Vector3f(vx_s, vy_s, vz_s);
+    space::r_m=Eigen::Vector3f(vx_m, vy_m, vz_m);
+    space::r_e = Eigen::Vector3f(vx_e, vy_e, vz_e);
+}
+
 using namespace space;
 
-void object::initialize_object(float_t x, float_t y, float_t z, float_t vx, float_t vy, float_t vz)
+void object::initialize_object(float x, float y, float z, int type)
 {
     object::x = x;
     object::y = y;
     object::z = z;
-    object::vx = vx;
-    object::vy = vy;
-    object::vz = vz;
+    switch(type)
+    {
+        case 1:
+            object::vx = vx_s;
+            object::vy = vy_s;
+            object::vz = vz_s;
+        case 2:
+            object::vx = vx_e;
+            object::vy = vy_e;
+            object::vz = vz_e;
+        case 3:
+            object::vx = vx_m;
+            object::vy = vy_m;
+            object::vz = vz_m;
+    }
 }
 void object::calculate(int type)
 {
     std::vector<float> data;
     T0 = T0 + dt;
-    Eigen::Vector3d a;
+    Eigen::Vector3f a;
     switch(type)
     {
         case 1:
-            a = (VectorF(m_s, m_e, r_s, r_e) + VectorF(m_s, m_m, r_s, r_m));
-            g = a/m_s;
+            g = ((VectorF(m_s, m_e, r_s, r_e) + VectorF(m_s, m_m, r_s, r_m))/m_s).cast<float>();
             v = v + g*dt;
             r_s = r_s + v*dt;
             data.insert(data.end(), {T0, r_s(0), r_s(1), r_s(2), vx, vy, vz});
             break;
         case 2:
-            g = (VectorF(m_s, m_e, r_e, r_s) + VectorF(m_s, m_m, r_e, r_m))/m_e;
+            g = ((VectorF(m_s, m_e, r_e, r_s) + VectorF(m_s, m_m, r_e, r_m))/m_e).cast<float>();
             v = v + g*dt;
             r_e = r_e + v*dt;
             data.insert(data.end(), {T0, r_e(0), r_e(1), r_e(2), vx, vy, vz});
             break;
         case 3:
-            g = (VectorF(m_s, m_e, r_m, r_s) + VectorF(m_s, m_m, r_m, r_e))/m_m;
+            g = ((VectorF(m_s, m_e, r_m, r_s) + VectorF(m_s, m_m, r_m, r_e))/m_m).cast<float>();
             v = v + g*dt;
             r_m = r_m + v*dt;
             data.insert(data.end(), {T0, r_m(0), r_m(1), r_m(2), vx, vy, vz});
@@ -76,13 +102,13 @@ void object::write_to_csv(int type)
         csv << '\n';
     }
 }
-Eigen::Vector3d VectorF(float m1, float m2, Eigen::Vector3f r1, Eigen::Vector3f r2)
+Eigen::Vector3d space::VectorF(float m1, float m2, Eigen::Vector3f r1, Eigen::Vector3f r2)
 {
     float t = G*(m1*m2);
     Eigen::Vector3f diff = r2 - r1;
-    return t * diff * std::pow(diff.norm(), -3);
+    return (t * diff * std::pow(diff.norm(), -3)).cast<double>();
 }
-std::vector<size_t> time(float t)
+std::vector<size_t> space::time(float t)
 {
     size_t day = size_t(t/86400);
     size_t hour = size_t((t-day*86400)/3600);
